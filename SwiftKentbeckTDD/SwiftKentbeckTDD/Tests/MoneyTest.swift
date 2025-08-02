a588b75c8e346b32e542b92d5bc93231c387eaff
sweetptios <sweetptios@gmail.com>
2025-08-02 16:12:14 +0900

    feat: p90 - 테스트에서 Dollor 대신 Money.dollor 메서드로 Dollor 객체 생성하도록 변경. 테스트 성공

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Tests/MoneyTest.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Tests/MoneyTest.swift
@@ -9,16 +9,16 @@ final class MoneyTest: AsyncSpec {
 
         it("test multiplication") {
             let five: Money = Money.dollor(5)
-            expect(five.times(2)).to(equal(Dollor(10)))
-            expect(five.times(3)).to(equal(Dollor(15)))
+            expect(five.times(2)).to(equal(Money.dollor(10)))
+            expect(five.times(3)).to(equal(Money.dollor(15)))
         }
 
         it("test equally") {
-            expect(Dollor(5) == Dollor(5)).to(beTrue())
-            expect(Dollor(5) == Dollor(6)).to(beFalsy())
+            expect(Money.dollor(5) == Money.dollor(5)).to(beTrue())
+            expect(Money.dollor(5) == Money.dollor(6)).to(beFalsy())
             expect(Franc(5) == Franc(5)).to(beTrue())
             expect(Franc(5) == Franc(6)).to(beFalsy())
-            expect(Franc(5)).notTo(equal(Dollor(5)))
+            expect(Franc(5)).notTo(equal(Money.dollor(5)))
         }
 
         it("test franc multiplication") {
