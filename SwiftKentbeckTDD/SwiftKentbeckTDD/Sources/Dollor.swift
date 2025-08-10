b4c59252550398ff0e2235674edf92fee621e362
sweetptios <sweetptios@gmail.com>
2025-08-10 12:17:51 +0900

    feat: p94 test currency - 하위클래스 제거를 위한 과정으로 두 클래스에서 동일한 currency 구현으로 리팩터링

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Dollor.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Dollor.swift
@@ -1,10 +1,18 @@
 final class Dollor: Money {
+    private let _currency: String
+
+    override init(_ value: Int) {
+        _currency = "USD"
+
+        super.init(value)
+    }
+
     override func times(_ multiplier: Int) -> Money {
         Dollor(amount * multiplier)
     }
 
     override func currency() -> String {
-        "USD"
+        _currency
     }
 }
 
--- SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Franc.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Franc.swift
@@ -1,9 +1,17 @@
 final class Franc: Money {
+    private let _currency: String
+
+    override init(_ value: Int) {
+        _currency = "CHF"
+
+        super.init(value)
+    }
+
     override func times(_ multiplier: Int) -> Money {
         Franc(amount * multiplier)
     }
 
     override func currency() -> String {
-        "CHF"
+        _currency
     }
 }
