66a731f8e09e3785d53212dadb8faceee68a9558
sweetptios <sweetptios@gmail.com>
2025-08-10 13:06:48 +0900

    feat: p106 - 하위 클래스에서 times() 제거 과정 - test different class equality 통과 - equals에서 type 대신 currency 비교하도록 변경

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Money.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Money.swift
@@ -31,7 +31,7 @@ class Money {
 
 extension Money: Equatable {
     static func == (lhs: Money, rhs: Money) -> Bool {
-        return lhs.amount == rhs.amount && type(of: lhs) == type(of: rhs)
+        return lhs.amount == rhs.amount && lhs.currency() == rhs.currency()
     }
 }
 
