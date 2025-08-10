f35f909a73d59fcbbd06e92434fe012bc80a66b8
sweetptios <sweetptios@gmail.com>
2025-08-10 17:23:44 +0900

    feat: p130 - reduce meney different currency 테스트 추가

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Tests/BankTest.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Tests/BankTest.swift
@@ -18,5 +18,12 @@ final class BankTest: AsyncSpec {
             let result = bank.reduce(Money.dollor(1), "USD")
             expect(result).to(equal(Money.dollor(1)))
         }
+
+        it("reduce money different currency") {
+            let bank = Bank()
+            bank.addRate("CHF", "USD", 2)
+            let result = bank.reduce(Money.franc(2), "USD")
+            expect(result) == Money.dollor(1)
+        }
     }
 }
