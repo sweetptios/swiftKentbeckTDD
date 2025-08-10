e0f39c3d65ecee8fad641f4427cc6a17e4d5b08e
sweetptios <sweetptios@gmail.com>
2025-08-10 17:06:28 +0900

    feat: p126 - reduce money 테스트 추가 - Bank의 reduce 내에 Money 타입의 분기처리를 추가하여 테스트 성공

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Bank.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Bank.swift
@@ -1,5 +1,8 @@
 final class Bank {
     func reduce(_ source: Expression, _ to: String) -> Money {
+        if let money = source as? Money {
+            return money
+        }
         guard let sum = source as? Sum
         else { return Money(0, currency: to) }
 
