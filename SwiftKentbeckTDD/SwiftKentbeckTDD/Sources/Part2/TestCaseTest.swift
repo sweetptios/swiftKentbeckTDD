933b166301abc4a353487853f8dcacd9b8d25c63
sweetptios <sweetptios@gmail.com>
2025-08-23 20:59:55 +0900

    test: p196 - setup 에서 TestResult 객체 생성하도록 하여 중복 제거

--- SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Part2/TestCaseTest.swift
+++ SwiftKentbeckTDD/SwiftKentbeckTDD/Sources/Part2/TestCaseTest.swift
@@ -2,29 +2,31 @@ import Foundation
 
 class TestCaseTest: TestCase {
     var test: WasRun!
+    var result: TestResult!
+
+    override func setUp() {
+        result = TestResult()
+    }
 
     @objc func testTemplateMethod() {
         test = WasRun("testMethod")
-        test.run(TestResult())
+        test.run(result)
         assert("setUp testMethod tearDown " == test.log)
     }
 
     @objc func testResult() {
         test = WasRun("testMethod")
-        let result = TestResult()
         test.run(result)
         assert("1 run, 0 failed" == result.summary())
     }
 
     @objc func testFailedResult() {
         test = WasRun("testBrokenMethod")
-        let result = TestResult()
         test.run(result)
         assert("1 run, 1 failed" == result.summary())
     }
 
     @objc func testFailedResultFormatting() {
-        let result = TestResult()
         result.testStarted()
         result.testFailed()
         assert("1 run, 1 failed" == result.summary())
@@ -34,7 +36,6 @@ class TestCaseTest: TestCase {
         let suite = TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        let result = TestResult()
         suite.run(result)
         assert("2 run, 1 failed" == result.summary())
     }
